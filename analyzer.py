58cf04c24633d6a019f46b032344b4fa6b920089
Barbod <barbodfazelii@gmail.com>
2026-07-10 21:58:22 +0330

    feat: add top 10 endpoints

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 PATTERN = re.compile(
     r'(?P<ip>\S+) - - \[(?P<time>.+)\] \"(?P<method>\S+) (?P<path>\S+) (?P<protocol>\S+)\" (?P<status>\d+) (?P<size>\d+) \"-\" \"(?P<user_agent>.+)\"'
@@ -15,16 +16,19 @@ def analyze(filePath):
         ips = set()
         lineCounter = 0
         errorCounter = 0
+        endpoints = Counter()
         for line in f:
             lineCounter+=1
             parsed = parse_line(line)
             if parsed:
                 ips.add(parsed.get("ip"))
+                endpoints[parsed.get("path")] += 1 
                 continue
             errorCounter+=1
     print(f"Total lines: {lineCounter}")
     print(f"Bad lines: {errorCounter}")
     print(f"Unique ips: {len(ips)}")
+    print(endpoints.most_common(10))
 
 
 
