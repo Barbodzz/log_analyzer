0219ee20ab760edd29be316bcf3cd41bf7de7ce9
Barbod <barbodfazelii@gmail.com>
2026-07-10 21:50:54 +0330

    feat: count unique IPs

--- analyzer.py
+++ analyzer.py
@@ -12,16 +12,19 @@ def parse_line(line):
 
 def analyze(filePath):
     with open(filePath) as f:
+        ips = set()
         lineCounter = 0
         errorCounter = 0
         for line in f:
             lineCounter+=1
             parsed = parse_line(line)
             if parsed:
+                ips.add(parsed.get("ip"))
                 continue
             errorCounter+=1
     print(f"Total lines: {lineCounter}")
     print(f"Bad lines: {errorCounter}")
+    print(f"Unique ips: {len(ips)}")
 
 
 
