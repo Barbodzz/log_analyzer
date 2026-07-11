81d3ab10224d57c03cdc8257ddabefc4b25302ee
Barbod <barbodfazelii@gmail.com>
2026-07-11 21:50:04 +0330

    feat: add suspicious IP detection for failed logins

--- analyzer.py
+++ analyzer.py
@@ -10,6 +10,7 @@ PATTERN = re.compile(
     r'(?P<ip>\S+) - - \[(?P<time>.+)\] \"(?P<method>\S+) (?P<path>\S+) (?P<protocol>\S+)\" (?P<status>\d+) (?P<size>\d+) \"-\" \"(?P<user_agent>.+)\"'
 )
 BAR = '\u2588'
+SUSPICIOUS_THRESHOLD = 50
 
 def parse_line(line):
     match = PATTERN.match(line)
@@ -33,16 +34,24 @@ def analyze(filePath):
         errorCounter = 0
         endpoints = Counter()
         times = Counter()
+        failedLoginRequests = Counter()
         for line in f:
             lineCounter+=1
             parsed = parse_line(line)
             if parsed:
-                ips.add(parsed.get("ip"))
-                endpoints[parsed.get("path")] += 1 
+                path = parsed.get("path")
+                ip = parsed.get("ip")
+                ips.add(ip)
+                endpoints[path] += 1 
                 times[parsed.get("time")[12:14]] += 1 # extract hour (index 12:14)
                 status = parsed.get("status")
+
+                if path == "/login" and status == "401":
+                    failedLoginRequests[ip] += 1
+
                 if is_error(status):
                     errorCounter+=1
+                
                 continue
             badLineCounter+=1 # malformed line, skip and count
 
@@ -55,21 +64,29 @@ def analyze(filePath):
     print(f"\nValid lines: {validLines}")
     print(f"\nUnique ips: {len(ips)}")
     print("\nMost common endpoints")
+
     header_ep = "endpoint"
     header_c = "count"
     print(f"{header_ep:<20} | {header_c}")
     for endpoint in endpoints.most_common(10):
         print(f"{endpoint[0]:<20} | {endpoint[1]}")
+
     print(f"\nError rate: {errorRate:.2f}%")
+
     print("\nTime distribution:")
     for hour in sorted(times.keys()):
         print(f"{hour}:00 | {times[hour]}")
+    
     print("Time distribution chart")
     maxValue = max(times.values())
     scale = maxValue / 25
     for hour in sorted(times.keys()):
         print(f"{hour}:00 | {BAR * int(times[hour] / scale)}")
 
+    print("Suspicious IPs")
+    for ip, count in failedLoginRequests.items():
+        if count > SUSPICIOUS_THRESHOLD:
+            print(f"{ip} -> {count} failed login attempts")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
